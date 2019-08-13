8257d36c02dba6a3fb09808911d98a6b17e90d27
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 00:57:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  array.to_s
+  string = ""
+  comma = ", "
+  and_string = "and "
+  count = 1
+  array.each do |el|
+    string << el 
+    count += 1 
+    
+    count == array.size - 1 ? string << and_string : string << comma
+  end
 end

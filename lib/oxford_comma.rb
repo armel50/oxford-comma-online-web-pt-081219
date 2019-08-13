3d12997075dff405e48d6f3b7e4ee40c23b012be
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 01:08:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  
-  
+  string = ""
+  count = 0
+  array.each do |el|
+    string << el
+    
+    count += 1
+    count == array.size - 1 ?  string << ",and " : string << ", "
+  end
+  string
 end

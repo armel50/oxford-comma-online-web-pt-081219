ec01f4a625b1002f3c6731b862781a64cbc33814
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 01:33:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     
     if array.size == 2 
       string = array.join(" and ")
-      elsif array.size > 2 
+      elsif array.size > 2 && count < array.size
         count == array.size - 1 ? string << ", and " : string << ", "
     end
     
